5688da25814616a268a158169979259c0497d9d0
Caijie <caijie96@gmail.com>
2020-10-25 05:50:44 +0800

    Fix styling

--- frontend/src/Pages/Interview/InterviewPage.js
+++ frontend/src/Pages/Interview/InterviewPage.js
@@ -15,8 +15,33 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import {CardContent} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const chatSocket = io('http://localhost:3000');
+const useStyles = makeStyles({
+  chatMessageContainer: {
+    overflowY: 'auto',
+    flexGrow: 1,
+    minHeight: 0,
+  },
+  interviewContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '64px 0',
+  },
+  leftPanel: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1
+  },
+  rightPanel: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+});
 
 function InterviewPage() {
   const history = useHistory();
@@ -25,6 +50,7 @@ function InterviewPage() {
   const [show, setShow] = useState(false);
   const [sendingMsg, setSendingMsg] = useState('');
   const [messages, setMessages] = useState([]);
+  const classes = useStyles();
   const sessionId = generateSessionId(user.email, match.email);
   useEffect(() => {
     chatSocket.on('connect', () =>
@@ -56,81 +82,77 @@ function InterviewPage() {
 
   return (
     <Layout>
-      <div className="inner-flex-top vert-center-sm">
-        <div className="main-50">
-          <Card className="container fixed-bg-question">
-            <CardContent>
-              <h3 className="display-5 text-center">Question</h3>
-              <p className="pt-3 text-left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardContent>
-              <h3>Chat</h3>
-              {/* Chat UI */}
-              <div className="chat-message-container" id="chat-message-container">
-                {messages.map((m, i) => (
-                  <div key={m.sender + m.msg + i} className={m.sender === user['nickname'] ? "chat-bubble-right" : "chat-bubble-left"}>
-                    <Typography variant="caption" style={{textTransform: 'capitalize'}} color="textSecondary">{m.sender}</Typography>
-                    <Chip label={m.msg}/>
-                  </div>
-                ))}
-              </div>
-              <div className="chat-text-field">
-                <TextField
-                  fullWidth
-                  value={sendingMsg}
-                  type="text"
-                  name="message"
-                  placeholder="Message"
-                  onChange={(e) => setSendingMsg(e.target.value)}
-                  onKeyUp={e => e.key === 'Enter' ? handleSend() : null}
-                />
-                <Button
-                  onClick={handleSend}
-                  type="submit"
-                  style={{ marginRight: '10px' }}
-                >
-                  Send
-                </Button>
-              </div>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="aside-50">
-          <Card className="text-center">
-            <CardContent>
-              <h3 className="display-5 mb-4">Collaborative NotePad</h3>
-              <form>
-                <textarea className="form-control textarea-minheight" rows="20" />
-              </form>
-            </CardContent>
-          </Card>
-
-          <div className="div-right">
-            <div className="div-right-adjust-5">
-              <Button
-                variant="contained"
-                color="secondary"
-                className="mt-5 pp-button"
-                onClick={handleShow}
-              >
-                End Session
-              </Button>
-            </div>
+      <Container style={{display: 'flex', height: 'calc(100vh - 77px)'}}>
+        <div className={classes.interviewContent}>
+          <div className={classes.leftPanel}>
+            <Card style={{display: 'flex', flex: 1}}>
+              <CardContent style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                <h3>Collaborative NotePad</h3>
+                <form style={{display: 'flex', flex: 1}}>
+                  <textarea className="form-control textarea-minheight" rows="20" />
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+          <div style={{width: 32}}/>
+          <div className={classes.rightPanel}>
+            <Card>
+              <CardContent>
+                <h3>Question</h3>
+                <p className="pt-3 text-left">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </CardContent>
+            </Card>
+            <Card style={{display: 'flex', flex: 1, margin: '32px 0'}}>
+              <CardContent style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                <h3>Chat</h3>
+                {/* Chat UI */}
+                <div className={classes.chatMessageContainer} id="chat-message-container">
+                  {messages.map((m, i) => (
+                    <div key={m.sender + m.msg + i} className={m.sender === user['nickname'] ? "chat-bubble-right" : "chat-bubble-left"}>
+                      <Typography variant="caption" style={{textTransform: 'capitalize'}} color="textSecondary">{m.sender}</Typography>
+                      <Chip label={m.msg}/>
+                    </div>
+                  ))}
+                </div>
+                <div className="chat-text-field">
+                  <TextField
+                    fullWidth
+                    value={sendingMsg}
+                    type="text"
+                    name="message"
+                    placeholder="Message"
+                    onChange={(e) => setSendingMsg(e.target.value)}
+                    onKeyUp={e => e.key === 'Enter' ? handleSend() : null}
+                  />
+                  <Button
+                    onClick={handleSend}
+                    type="submit"
+                    style={{ marginRight: '10px' }}
+                  >
+                    Send
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleShow}
+            >
+              End Session
+            </Button>
           </div>
         </div>
         <EndSessionModal handleClose={handleClose} show={show} />{' '}
-      </div>
+      </Container>
     </Layout>
   );
 }
