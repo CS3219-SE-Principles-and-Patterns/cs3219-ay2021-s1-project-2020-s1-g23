223579be42c92c32b2f4d4a6f4853fb00d0695ee
Caijie <caijie96@gmail.com>
2020-10-27 19:24:36 +0800

    Set up chatms for deployment

--- backend/chat/index.js
+++ backend/chat/index.js
@@ -8,18 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server).of('/chat');
 
-app.get('/', () => console.log('Chat microservice is working!'));
+app.get('/chat', () => console.log('Chat microservice is working!'));
 
 io.on('connection', socket => {
-  console.log('a user connected');
-  socket.on('disconnect', () => console.log('user disconnected'));
   socket.on('newMessage', msg => {
     io.emit(msg.sessionId, msg.payload);
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on port 3000...');
+const port = process.env.PORT || 8080
+server.listen(port, () => {
+  console.log(`Chat ms listening on port ${port}...`);
 });
