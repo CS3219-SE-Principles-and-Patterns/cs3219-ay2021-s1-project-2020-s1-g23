74f153db80dbb0948a888d7a42d28467da4b1927
keith <keithtxhh@gmail.com>
2020-11-01 02:22:45 +0800

    Remove old code

--- backend/editor/server.js
+++ backend/editor/server.js
@@ -26,34 +26,3 @@ const port = process.env.PORT || 4001;
 server.listen(port, () => {
   console.log(`Editor ms listening on port ${port}...`);
 });
-
-// TO DELETE:
-
-// const express = require("express");
-// const http = require("http");
-// const socketIo = require("socket.io");
-// const axios = require("axios");
-// const port = process.env.PORT || 4001;
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-// const getQuestion = require("./question-generator");
-
-// app.get("/", (req, res) =>
-//   res.status(200).json({ message: "ok", data: "Editor microservice is working" })
-// );
-// const getApiAndEmit = async socket => {
-//   try {
-//     const qn = getQuestion();
-//     socket.emit("FromAPI", qn.question, qn.input, qn.output); // Emitting a new message. It will be consumed by the client
-//   } catch (error) {
-//     console.error(`Error: ${error.code}`);
-//   }
-// };
-
-// io.on("connection", socket => {
-//   console.log("New client connected"), setInterval(() => getApiAndEmit(socket), 1000);
-//   socket.on("disconnect", () => console.log("Client disconnected"));
-// });
-
-// server.listen(port, () => console.log(`Listening on port ${port}`));
