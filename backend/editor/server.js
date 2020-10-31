bff7ae0e052af3d9b337de35b8cf9d40a5ad5b73
keith <keithtxhh@gmail.com>
2020-11-01 00:17:48 +0800

    Replace api endpoints with socket programming

--- backend/editor/server.js
+++ backend/editor/server.js
@@ -1,92 +1,59 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 3123;
 const cors = require("cors");
 
+const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Redis set up
-const redis = require("redis");
-const redisHostname = process.env.REDIS_HOST || "localhost";
-const redisPort = process.env.REDIS_PORT || 6379;
-const redisSecret = process.env.REDIS_SECRET || "password";
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-const client = redis.createClient(redisPort, redisHostname, { no_ready_check: true });
-client.auth(redisSecret);
-client.on("error", err => console.log("Error " + err));
-client.on("connect", () => console.log("Connected to Redis"));
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "ok", data: "Editor microservice is working" })
+);
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-// Socket.io
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-io.on("connection", () => {
-  console.log("a user is connected");
-});
-
-/**
- * Retrieves question and set sessionKey in redis
- * @param req.body.sessionKey
- * @returns question
- */
-app.post("/connect", (req, res) => {
-  const sessionKey = req.body.sessionKey;
-  client.get(sessionKey, (err, reply) => {
-    // reply will be null if sessionKey does not exist
-    if (reply !== null) {
-      res.send(400, "Error: Session key is already in use! Maybe try /update?");
-    } else {
-      const qn = "Reverse linked list"; // placeholder
-      console.log("New connection, retrieving question: " + qn);
-      const sessionObj = { question: qn, text: "" };
-      client.set([sessionKey, JSON.stringify(sessionObj)], () => res.send(201, qn));
-    }
+const getQuestion = require("./question-generator");
+io.on("connection", socket => {
+  const qn = getQuestion();
+  socket.emit("Question", qn.question, qn.input, qn.output);
+  socket.on("newMessage", msg => {
+    io.emit(msg.sessionId, msg.payload);
   });
 });
-
-/**
- * Updates code body
- * @param req.body.sessionKey
- * @param req.body.codeBody
- * @returns status code 202 if accepted
- */
-app.post("/update", (req, res) => {
-  const sessionKey = req.body.sessionKey;
-  const codeBody = req.body.codeBody;
-  client.get(sessionKey, (err, reply) => {
-    // reply will be null if sessionKey does not exist
-    if (reply === null) {
-      res.send(400, "Error: Session key not found, maybe try /connect?");
-    } else {
-      console.log("Updating code body: " + codeBody);
-      const sessionObj = { question: reply.question, text: codeBody };
-      client.set([sessionKey, JSON.stringify(sessionObj)], () => res.sendStatus(202));
-    }
-  });
+const port = process.env.PORT || 4001;
+server.listen(port, () => {
+  console.log(`Editor ms listening on port ${port}...`);
 });
 
-/**
- * Retrieves code
- * @param req.body.sessionKey
- * @returns code in string form
- */
-app.post("/code", (req, res) => {
-  const sessionKey = req.body.sessionKey;
-  client.get(sessionKey, (err, reply) => {
-    // reply will be null if sessionKey does not exist
-    if (reply === null) {
-      res.send(400, "Error: Session key not found, maybe try /connect?");
-    } else {
-      const sessionObj = JSON.parse(reply);
-      console.log("Retrieving code body: " + sessionObj.text);
-      res.send(sessionObj.text);
-    }
-  });
-});
+// TO DELETE:
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+// const axios = require("axios");
+// const port = process.env.PORT || 4001;
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server);
+// const getQuestion = require("./question-generator");
+
+// app.get("/", (req, res) =>
+//   res.status(200).json({ message: "ok", data: "Editor microservice is working" })
+// );
+// const getApiAndEmit = async socket => {
+//   try {
+//     const qn = getQuestion();
+//     socket.emit("FromAPI", qn.question, qn.input, qn.output); // Emitting a new message. It will be consumed by the client
+//   } catch (error) {
+//     console.error(`Error: ${error.code}`);
+//   }
+// };
+
+// io.on("connection", socket => {
+//   console.log("New client connected"), setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => console.log("Client disconnected"));
+// });
+
+// server.listen(port, () => console.log(`Listening on port ${port}`));
