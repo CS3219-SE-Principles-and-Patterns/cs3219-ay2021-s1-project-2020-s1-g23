aecc9f9548c84869917d47f14d415fc4c63dd3d2
Permas Teo <permasteo@gmail.com>
2020-11-11 01:47:09 +0800

    update copywriting

--- frontend/src/Pages/Home/HomePageUser.js
+++ frontend/src/Pages/Home/HomePageUser.js
@@ -89,9 +89,7 @@ const HomePageUser = ({ user }) => {
               <h1 className="display-5 text-blue">{rank}</h1>{' '}
               {/* TODO: Define constants to map ELO to rank */}
               <h3 className="pt-3 pb-5 margin-lr">
-                You are
-                <span className="text-strong"> xxxx </span>
-                points away from the next rank. Keep it up!
+                You are doing well! Keep up the prep!
               </h3>
               <Link to="/history">
                 <Button
