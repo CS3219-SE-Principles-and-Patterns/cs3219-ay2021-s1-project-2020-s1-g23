d8784dfbac97ffdc6521a0fb5b1d524ebeda6fba
Nicolas <nicolas_wee@hotmail.com>
2020-11-10 19:02:54 +0800

    create cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,73 +1,73 @@
-# locals {
-#   s3_origin_id = "myS3Origin"
-# }
+locals {
+  s3_origin_id = "myS3Origin"
+}
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.react.bucket_regional_domain_name
-#     origin_id   = local.s3_origin_id
-#   }
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "peerprep.live.s3.amazonaws.com"
+    origin_id   = local.s3_origin_id
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
-#   aliases = ["simp.social"]
+  aliases = ["peerprep.live"]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     lambda_function_association {
-#       event_type   = "origin-response"
-#       include_body = false
-#       lambda_arn   = "arn:aws:lambda:us-east-1:650965765717:function:CloudFront-CSP:1"
-#     }
+    lambda_function_association {
+      event_type   = "viewer-response"
+      include_body = false
+      lambda_arn   = var.lambda_edge
+    }
 
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#   }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+  }
 
-#   price_class = "PriceClass_All"
+  price_class = "PriceClass_All"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-#   tags = {
-#     Environment = "production"
-#   }
+  tags = {
+    Environment = "production"
+  }
 
-#   viewer_certificate {
-#     acm_certificate_arn = "arn:aws:acm:us-east-1:650965765717:certificate/0e9f487c-22ca-422e-9430-650f157a0aeb"
-#     ssl_support_method = "sni-only"
-#   }
+  viewer_certificate {
+    acm_certificate_arn = var.acm_certificate
+    ssl_support_method = "sni-only"
+  }
 
-#   custom_error_response {
-#     error_code = 403
-#     error_caching_min_ttl = 300
-#     response_page_path = "/index.html"
-#     response_code = 200
-#   }
+  custom_error_response {
+    error_code = 403
+    error_caching_min_ttl = 300
+    response_page_path = "/index.html"
+    response_code = 200
+  }
 
-#   custom_error_response {
-#     error_code = 404
-#     error_caching_min_ttl = 300
-#     response_page_path = "/index.html"
-#     response_code = 200
-#   }
-# }
+  custom_error_response {
+    error_code = 404
+    error_caching_min_ttl = 300
+    response_page_path = "/index.html"
+    response_code = 200
+  }
+}
